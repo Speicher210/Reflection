55d8a51c4826aab945a910c7c42a3769cdb07352
Andrej Kabachnik <andrej_ka@gmx.de>
2017-02-03 09:47:01 +0100

    Custom trim patterns in getReflectionDocComment() (#2)

--- src/ReflectionDocComment.php
+++ src/ReflectionDocComment.php
@@ -35,8 +35,9 @@ class ReflectionDocComment
      * Constructor.
      *
      * @param string $comment The original document block comment.
+     * @param string $trimLinePattern Pattern for trim() function applied to each line. Usefull to leave spaces or tabs. The default is the same as calling trim() without the argument.
      */
-    public function __construct($comment)
+    public function __construct($comment, $trimLinePattern = " \t\n\r\0\x0B")
     {
         $this->originalDocBlock = trim((string)$comment);
 
@@ -46,9 +47,9 @@ class ReflectionDocComment
         if (isset($comment[0]) === true) {
             $description = $comment[0];
             $description = preg_split("/\n|\n\r/", $description);
-            array_walk($description, function (& $value) {
-                $value = trim($value);
-            });
+            array_walk($description, function (& $value, $key, $trimLinePattern) {
+                $value = trim($value, $trimLinePattern);
+            }, $trimLinePattern);
 
             foreach ($description as $key => $descLine) {
                 if ($descLine !== '') {
--- src/ReflectionDocCommentTrait.php
+++ src/ReflectionDocCommentTrait.php
@@ -17,13 +17,14 @@ trait ReflectionDocCommentTrait
 
     /**
      * Get the document of the method.
-     *
+     * 
+     * @param string $trimLinePattern Pattern for trim() function applied to each line. Usefull to leave spaces or tabs. The default is the same as calling trim() without the argument.
      * @return \Wingu\OctopusCore\Reflection\ReflectionDocComment
      */
-    public function getReflectionDocComment()
+    public function getReflectionDocComment($trimLinePattern = " \t\n\r\0\x0B")
     {
         if ($this->reflectionDocComment === null) {
-            $this->reflectionDocComment = new ReflectionDocComment((string)$this->getDocComment());
+            $this->reflectionDocComment = new ReflectionDocComment((string)$this->getDocComment(), $trimLinePattern);
         }
 
         return $this->reflectionDocComment;
